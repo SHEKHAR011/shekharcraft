67d2566f2ae584c8a05a166de545aa57b33dab11
SHEKHAR <shekhar3203@gmail.com>
2025-05-20 11:55:53 +0530

    Project tile arrangement

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -12,9 +12,18 @@ export interface Project {
 }
 
 export const projectData: Project[] = 
-[
-  {
+[{
     id: 1,
+    title: 'Music Streaming App with Dezzer',
+    description: 'Complete visual identity redesign for a travel agency, including logo, website, and marketing materials.',
+    image: 'https://images.pexels.com/photos/7412095/pexels-photo-7412095.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    category: 'mobile',
+    technologies: ['Native Android', 'Dezzer Api', 'Picasso','Glide','Material UI','Json Converter'],
+    github: 'htsdad',
+    // liveUrl: '#'
+  },
+  {
+    id: 2,
     title: 'Smart Traffic Light Simulation',
     description: 'A dynamic and realistic simulation of a 4-way Indian intersection using Python and Pygame! This project demonstrates a traffic light system with roundabout logic, phase cycling, and vehicles that obey signals.',
     image: ScreenShot, 
@@ -24,7 +33,7 @@ export const projectData: Project[] =
     //liveUrl: '#'
   },
   {
-    id: 2,
+    id: 3,
     title: 'Discord Bot with Gemini Integration',
     description: 'A versatile Discord bot powered by Python and the Gemini API, offering conversational AI features for smart automation and engaging real-time interactions.',
     image: 'https://images.pexels.com/photos/3927410/pexels-photo-3927410.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
@@ -34,7 +43,7 @@ export const projectData: Project[] =
     
   },
   {
-    id: 3,
+    id: 4,
     title: 'Adventure Game',
     description: 'If Elif Else Adventure Game in Python',
     image: 'https://images.pexels.com/photos/7567460/pexels-photo-7567460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
@@ -43,16 +52,7 @@ export const projectData: Project[] =
     github: 'https://github.com/SHEKHAR011/Adventure-Game',
     
   },
-  {
-    id: 4,
-    title: 'Music Streaming App with Dezzer',
-    description: 'Complete visual identity redesign for a travel agency, including logo, website, and marketing materials.',
-    image: 'https://images.pexels.com/photos/7412095/pexels-photo-7412095.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    category: 'mobile',
-    technologies: ['Native Android', 'Dezzer Api', 'Picasso','Glide','Material UI','Json Converter'],
-    github:'sdfdsf',
-    // liveUrl: '#'
-  },
+  
   // {
   //   id: 5,
   //   title: 'Real Estate Listing Platform',
